cca1d2d8dad1183f69c28f92ffe74c101b0b9882
King <11500472+valvuong2201@users.noreply.github.com>
2023-01-21 15:34:06 +0700

    initial comment

---
+++ server/server.js
@@ -0,0 +1,94 @@
+import fastify from "fastify"
+import sensible from "@fastify/sensible"
+import cors from '@fastify/cors'
+import cookie from '@fastify/cookie'
+import dotenv from "dotenv"
+import { PrismaClient } from "@prisma/client"
+dotenv.config()
+
+const app = fastify()
+const CURRENT_USER_ID = (await prisma.user.findFirst({
+    where: {
+        name: 'Kyle'
+    }
+})).id
+app.register(sensible)
+app.register(cookie, { secret: process.env.COOKIE_SECRET })
+app.register(cors, {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+})
+app.addHook('onRequest', (req, res, done) => {
+    if (req.cookies.userId !== CURRENT_USER_ID) {
+        req.cookies.userId = CURRENT_USER_ID
+        res.clearCookie('userId')
+        res.setCookie('userId', CURRENT_USER_ID)
+    }
+    done()
+})
+const prisma = new PrismaClient()
+const COMMENT_SELECT_FIELDS = {
+    id: true,
+    message: true,
+    parentId: true,
+    createdAt: true,
+    user: {
+        select: {
+            id: true,
+            name: true
+        }
+    }
+}
+
+app.get("/posts", async (req, res) => {
+    return await commitToDb(prisma.post.findMany({
+        select: {
+            id: true,
+            title: true
+        }
+    }))
+})
+
+app.get("/posts/:id", async (req, res) => {
+    return await commitToDb(prisma.post.findUnique({
+        where: {
+            id: req.params.id
+        },
+        select: {
+            body: true,
+            title: true,
+            comments: {
+                orderBy: {
+                    createdAt: "desc"
+                },
+                select: COMMENT_SELECT_FIELDS
+            }
+        }
+    }))
+})
+
+app.post('/posts/:id/comments', async (req, res) => {
+    if (req.body.message === '' || req.body.message == null) {
+        return res.send(app.httpErrors.badRequest('Message is required'))
+    }
+
+    return await commitToDb(
+        prisma.comment.create({
+            data: {
+                message: req.body.message,
+                userId: req.cookies.userId,
+                parentId: req.body.parentId,
+                postId: req.params.id
+            },
+            select: COMMENT_SELECT_FIELDS,
+        })
+    )
+})
+
+const commitToDb = async (promise) => {
+    const [error, data] = await app.to(promise)
+    if (error) return app.httpErrors.internalServerError(error.message)
+    return data
+}
+
+app.listen({ port: process.env.PORT })
